d392bec336569d46b4047a5b455b7fd125ed3f35
mayank-000 <sagarmayank483@gmail.com>
2026-03-12 16:13:44 +0530

    little

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 const app = express();
+app.use(express.json())
 import { connectDB } from './config/db.js';
 
 connectDB();
+
+app.use('/api/todo', todoRouter);
+app.use('/api/users', userRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
