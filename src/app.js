aaab44f572552b8b791f461a081be16aea773e8a
Mayank Sagar <mayanksagar159@gmail.com>
2026-03-24 19:56:05 +0530

    Configure CORS with environment variable for frontend

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
 import { connectDB } from './config/db.js';
 
 connectDB();
